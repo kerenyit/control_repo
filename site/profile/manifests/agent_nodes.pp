5097d92edda975958342c8ec39c8adca884cc47c
kereny <34091014+kerenyit@users.noreply.github.com>
2022-02-17 13:49:07 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent 
   dockeragent::node {'web.pupper.vm':}
   dockeragent::node {'db.pupper.vm':}
+  host {'web.puppet.vm':
+    ensure => present,
+    ip     => '172.18.0.2'
+  }
+  host {'db.puppet.vm':
+    ensure => present,
+    ip     => '172.18.0.3'
+  }
 }
